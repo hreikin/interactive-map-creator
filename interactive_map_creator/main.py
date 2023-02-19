7aab4b90d4ae01aec2fba1c4097bbfa3ee01f88b
Michael Haslam <hreikin@gmail.com>
2023-02-19 22:18:49 +0000

    Add some basic comments to explain each section.

--- interactive_map_creator/main.py
+++ interactive_map_creator/main.py
@@ -3,6 +3,7 @@ from streamlit_option_menu import option_menu
 import streamlit_authenticator as st_auth
 from apps import home, create, library, settings
 
+# Configure page title, layout, menu items and links.
 st.set_page_config(
     page_title="Interactive Map Creator",
     layout="wide",
@@ -19,7 +20,6 @@ st.set_page_config(
 
 # A dictionary of apps in the format of {"func": foo, "title": "foo", "icon": "bootstrap-icon-name"}
 # More icons can be found here: https://icons.getbootstrap.com
-
 apps = [
     {"func": home.app, "title": "Home", "icon": "house"},
     {"func": create.app, "title": "Create", "icon": "file-earmark-plus"},
@@ -38,6 +38,7 @@ if "page" in params:
 else:
     default_index = 0
 
+# Load streamlit-authenticator config file and instantiate authenticator.
 with open(".streamlit/config.yaml") as file:
     config = st_auth.yaml.load(file, Loader=st_auth.SafeLoader)
 
@@ -49,15 +50,17 @@ authenticator = st_auth.Authenticate(
     config["preauthorized"]
 )
 
+# Show login form if user isn't logged in.
 if st.session_state["authentication_status"] == None:
-    # st.header("Interactive Map Creator")
-    # Login form
     name, authentication_status, username = authenticator.login("Login", "main")
+    
+# Show error message if authentication fails and reset authentication_status to
+# none to avoid issues with forms disappearing after the user tries to log in.
 if st.session_state["authentication_status"] == False:
     st.error("Username/password is incorrect.")
     st.session_state["authentication_status"] = None
 
-
+# Forgotten ussername and password forms.
 if st.session_state["authentication_status"] == None:
     st.info("If you have forgotten your username or password, then please use one of the forms below.")
     col_1, col_2 = st.columns(2)
@@ -72,7 +75,6 @@ if st.session_state["authentication_status"] == None:
         except Exception as e:
             st.error(e)
     with col_2:
-        # Forgotten password form
         username_forgot_pw, email_forgot_password, random_password = authenticator.forgot_password("Forgotten Password ?")
         try:
             if username_forgot_pw:
@@ -82,9 +84,9 @@ if st.session_state["authentication_status"] == None:
                 st.error("Username not found.")
         except Exception as e:
             st.error(e)
-    
+
+# Registration form.
 if st.session_state["authentication_status"] == None:
-    # Registration form
     st.warning("If you would like to register for an account then please use the form below.")
     try:
         if authenticator.register_user("New User Registration", preauthorization=True):
@@ -93,6 +95,7 @@ if st.session_state["authentication_status"] == None:
         st.error(e)
 
 with st.sidebar:
+    # Sidebar menu from streamlit-option-menu
     selected = option_menu(
         "Interactive Map Creator",
         options=titles,
@@ -102,6 +105,7 @@ with st.sidebar:
     )
     col_1_sidebar, col_2_sidebar = st.columns(2)
 
+# If user is logged in then display a message and button to logout.
 with col_1_sidebar:
     if st.session_state["authentication_status"]:
         authenticator.logout("Logout", "main")
@@ -109,6 +113,7 @@ with col_2_sidebar:
     if st.session_state["authentication_status"]:
         st.write(f"Welcome *{st.session_state['name']}*")
 
+# When the user makes a menu selection run the relevant app.
 for app in apps:
     if app["title"] == selected:
         app["func"]()

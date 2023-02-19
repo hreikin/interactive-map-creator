7410a565041a5f7e94c9cfc1a1de1c95aacef148
Michael Haslam <hreikin@gmail.com>
2023-02-19 23:23:06 +0000

    Use authenticator from session_state to provide simple change details and password forms.

--- interactive_map_creator/apps/settings.py
+++ interactive_map_creator/apps/settings.py
@@ -4,5 +4,14 @@ def app():
 
     if st.session_state["authentication_status"]:
         st.header("Settings")
-        st.write(f"Welcome *{st.session_state['name']}*.")
-        st.write("Content to show to authenticated users.")
+        st.subheader(f"Welcome *{st.session_state['name']}*.")
+    try:
+        if st.session_state["authenticator"].update_user_details(st.session_state["username"], "Update User Details"):
+            st.success("Entries updated successfully.")
+    except Exception as e:
+        st.error(e)
+    try:
+        if st.session_state["authenticator"].reset_password(st.session_state["username"], "Change Password"):
+            st.success("Password modified successfully.")
+    except Exception as e:
+        st.error(e)

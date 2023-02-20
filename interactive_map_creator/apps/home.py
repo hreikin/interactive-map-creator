034f9f04165f161e8cdc533a22b787d16c1b9713
Michael Haslam <hreikin@gmail.com>
2023-02-20 21:20:04 +0000

    Add lorem ipsum to help with visuals and create skeleton version of page. Create a function to load images of example maps in the sidebar and cache the result.

--- interactive_map_creator/apps/home.py
+++ interactive_map_creator/apps/home.py
@@ -1,5 +1,29 @@
 import streamlit as st
+from pathlib import Path
+
+@st.cache_data(show_spinner="Fetching images...")
+def load_images(images=list()):
+    for img in images:
+        st.image(str(img.resolve()))
 
 def app():
-
-    st.header(f"Welcome.")
+    col_1, col_2 = st.columns(2)
+    with col_1:
+        st.header(f"*Welcome*, to the Interactive Map Creator.")
+        st.markdown(
+            """
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            """
+            )
+        st.subheader("Getting Started")
+        st.markdown(
+            """
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+            """
+            )
+    sidebar_images = [
+        Path("assets/images/example_map_01.png"),
+        Path("assets/images/example_map_02.png"),
+    ]
+    with col_2:
+        load_images(sidebar_images)

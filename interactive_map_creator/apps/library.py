5cb8e78bcedebae37144da38d04bae5154df5eea
Michael Haslam <hreikin@gmail.com>
2023-02-22 14:53:22 +0000

    Fix utils import and remove clear cache function unil it is needed.

--- interactive_map_creator/apps/library.py
+++ interactive_map_creator/apps/library.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from interactive_map_creator import utils
+import utils
 
 def app():
     with st.sidebar:
@@ -11,7 +11,6 @@ def app():
         uploaded_file = st.file_uploader("Upload New File", type=["png", "jpg", "jpeg"], help="Upload a new image to use as map tiles.")
         if uploaded_file is not None:
             utils.save_uploaded_file(uploaded_file, utils.uploads_folder)
-            st.cache_data.clear()
     st.header("Library")
     maps_dropdown = st.expander("**Created Maps**", expanded=True)
     source_image_dropdown = st.expander("**Uploaded Images**", expanded=True)

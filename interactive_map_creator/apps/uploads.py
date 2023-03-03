f648809fb73c8eb798c82140e369c67d69fccb3f
Michael Haslam <hreikin@gmail.com>
2023-03-03 20:21:59 +0000

    Update references to uploads/library with new name.

--- interactive_map_creator/apps/uploads.py
+++ interactive_map_creator/apps/uploads.py
@@ -13,7 +13,7 @@ def app():
             )
         uploaded_file = st.file_uploader("Upload New File", type=["png", "jpg", "jpeg"], help="Upload a new image to use as map tiles.")
         if uploaded_file is not None:
-            utils.save_uploaded_file(uploaded_file, utils.uploads_folder)
+            utils.save_uploaded_file(uploaded_file, utils.library_folder)
     maps_dropdown = st.expander("**Created Maps**", expanded=True)
     source_image_dropdown = st.expander("**Uploaded Images**", expanded=True)
     with maps_dropdown:
@@ -23,4 +23,4 @@ def app():
     with source_image_dropdown:
         source_gallery = st.container()
     with source_gallery:
-        utils.create_gallery(utils.uploads_folder)
+        utils.create_gallery(utils.library_folder)

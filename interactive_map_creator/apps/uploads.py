f463a4dae594397f4adbad61384b864376c4cf09
Michael Haslam <hreikin@gmail.com>
2023-03-14 21:31:37 +0000

    Create two file uploaders and two galleries in expanders.

--- interactive_map_creator/apps/uploads.py
+++ interactive_map_creator/apps/uploads.py
@@ -1,21 +1,22 @@
 import streamlit as st
 import logging
 import utils
-from streamlit_simple_gallery import ImageGallery
-from gallery import MapGallery
+from streamlit_uploads_library.library import Library
+from streamlit_uploads_library.uploads import UploadFiles
 from tiles import CreateTiles
-from uploading import UploadFiles
 
 logger = logging.getLogger(__name__)
 
 def app():
     with st.sidebar:
         sidebar_info_msg = st.info("Upload new images or icons and view your previously uploaded files and created tile sets.")
-        upload_files = UploadFiles()
+        with st.expander(label="Upload Files", expanded=True):
+            upload_images = UploadFiles(save_location="library/images/", header="Images", info_msg="Upload images to create tiles from here.", uid="images", upload_label="Upload Images")
+            upload_icons = UploadFiles(save_location="library/icons/", header="Icons", info_msg="Upload custom icons to use on your map here.", uid="icons", upload_label="Upload Icons")
         create_tiles = CreateTiles()
-    tiles_gallery = MapGallery(directory=utils.tiles_folder, label="**Tiles**")
-    # tiles_gallery.create_gallery()
-    images_gallery = ImageGallery(directory=utils.images_folder)
-    # images_gallery.create_gallery()
-    icons_gallery = ImageGallery(directory=utils.icons_folder, label="**Icons**")
-    # icons_gallery.create_gallery()
+    images_expander = st.expander(label="Images", expanded=True)
+    icons_expander = st.expander(label="Icons", expanded=True)
+    with images_expander:
+        images_library = Library(directory=utils.images_folder, uid="images")
+    with icons_expander:
+        icons_library = Library(directory=utils.icons_folder, uid="icons")

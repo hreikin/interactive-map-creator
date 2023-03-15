7f3d15eaa7c8df1c0ca1983167153b73414332c4
Michael Haslam <hreikin@gmail.com>
2023-03-15 22:06:05 +0000

    Refactor and design work.
    Standardize variable names.
    Create add/edit/remove layer options.
    Create edit/remove marker options.
    Create skeleton of add/edit/remove zone options.

--- interactive_map_creator/mapmaking.py
+++ interactive_map_creator/mapmaking.py
@@ -19,22 +19,42 @@ class CreateMap():
                     st.error("Please provide a name for the map in the 'Map Options' section and click apply before saving again.")
 
         if self.map_options_btn:
-            if len(self.map_name) > 0:
-                with self.map_name_warning_msg:
+            if len(self.map_options_map_name) > 0:
+                with self.map_options_name_warning_msg:
                     st.empty()
-                st.session_state["map_name"] = self.map_name
+                st.session_state["map_name"] = self.map_options_map_name
                 st.session_state["base_layer"] = self.base_layer
                 st.session_state["map_options_min_zoom"] = self.map_options_min_zoom
                 st.session_state["map_options_max_zoom"] = self.map_options_max_zoom
             else:
-                with self.map_name_warning_msg:
+                with self.map_options_name_warning_msg:
                     st.error("Please provide a name for the map and click apply.")
 
+        if self.layer_options_add_btn:
+            # Do something
+            pass
+        if self.layer_options_edit_btn:
+            # Do something
+            pass
+        if self.layer_options_remove_btn:
+            # Do something
+            pass
         if self.marker_options_add_btn:
             # Do something
             pass
-
-        if self.zone_options_btn:
+        if self.marker_options_edit_btn:
+            # Do something
+            pass
+        if self.marker_options_remove_btn:
+            # Do something
+            pass
+        if self.zone_options_add_btn:
+            # Do something
+            pass
+        if self.zone_options_edit_btn:
+            # Do something
+            pass
+        if self.zone_options_remove_btn:
             # Do something
             pass
 
@@ -77,8 +97,12 @@ class CreateMap():
             st.session_state["last_clicked"] = {'lat': 0, 'lng': 0}
             # Markers list or use FeatureGroup for removing markers ? This just allows the selector in the 
             # "Remove" markers tab to load at present.
+            self.layers_list = list()
+            st.session_state["layers_list"] = self.layers_list
             self.markers_list = list()
             st.session_state["markers_list"] = self.markers_list
+            self.zones_list = list()
+            st.session_state["zones_list"] = self.zones_list
 
     def create_sidebar(self):
         # General sidebar layout
@@ -99,42 +123,101 @@ class CreateMap():
         with self.create_map_options:
             self.create_map_options_msg = st.info("Create your map by defining the options available below, the map will update to reflect your changes.")
             self.map_options_form = st.form(key="Map Options")
-            self.marker_options_form = st.form(key="Marker Options")
-            self.zone_options_form = st.form(key="Zone Options")
+            self.layer_options_form = st.form(key="Layers")
+            self.marker_options_form = st.form(key="Markers")
+            self.zone_options_form = st.form(key="Zones")
         # Map options form
         with self.map_options_form:
             self.map_options_subheader = st.subheader("Map Options")
-            self.map_name_warning_msg = st.empty()
-            self.map_name = st.text_input(label="Map Name", placeholder="Select a name for your map", help="Select a name for your map, this will also be the base layers name in the layers control on the map.")
+            self.map_options_info_msg = st.info("Configure your maps default values.")
+            self.map_options_name_warning_msg = st.empty()
+            self.map_options_map_name = st.text_input(label="Map Name", placeholder="Select a name for your map.", help="Select a name for your map, this will also be the base layers name in the layers control on the map.")
             self.base_layer = st.selectbox(label="Base Layer", options=st.session_state["library_tiles"], help="Choose tiles from the library to use as the base layer.")
             self.map_options_min_zoom = st.selectbox(label="Min Zoom", options=range(1,21), index=st.session_state["map_options_min_zoom"], help="Select minimum zoom for the map. This should be the same as the 'Min Zoom' setting used when creating the tiles or higher.")
             self.map_options_max_zoom = st.selectbox(label="Max Zoom", options=range(1,21), index=st.session_state["map_options_max_zoom"], help="Select maximum zoom for the map. This should be the same as the 'Max Zoom' setting used when creating the tiles or lower.")
             self.map_options_btn = st.form_submit_button(label="Apply", type="primary", use_container_width=True)
+        # Layer options form
+        with self.layer_options_form:
+            self.layer_options_subheader = st.subheader("Layers")
+            self.layer_options_info_msg = st.info("Select the current active layer to add all markers and zones too.")
+            self.layer_options_current_layer = st.selectbox(label="Current Acive Layer", options=st.session_state["layers_list"], help="Select the current active layer to add all markers and zones too.")
+            self.layer_options_add_tab, self.layer_options_edit_tab, self.layer_options_remove_tab = st.tabs(["Add", "Edit", "Remove"])
+        # Add tab
+        with self.layer_options_add_tab:
+            self.layer_options_add_info_msg = st.info("Give your new layer a name and define its position in the layers controller.")
+            self.layer_options_add_layer_name = st.text_input(label="New Layer Name", key="add-layer-name", placeholder="Select a name for your layer.", help="Select a name for your layer, this will also be the layers name in the layers control on the map.")
+            self.layer_options_add_position = st.number_input(label="Position", key="add-layer-position", min_value=1)
+            self.layer_options_add_btn = st.form_submit_button(label="Add", type="primary", use_container_width=True)
+        # Edit tab
+        with self.layer_options_edit_tab:
+            self.layer_options_edit_info_msg = st.info("Edit the selected layers name and position in the layers controller.")
+            self.layer_options_edit_selected_layer = st.selectbox(label="Layer To Edit", options=st.session_state["layers_list"], help="Select the layer you wish to edit.")
+            self.layer_options_edit_layer_name = st.text_input(label="Layer Name", key="edit-layer-name", placeholder="Select a name for the layer.", help="Select a name for your layer, this will also be the layers name in the layers control on the map.")
+            self.layer_options_edit_layer_position = st.number_input(label="Position", key="edit-layer-position", min_value=1)
+            self.layer_options_edit_btn = st.form_submit_button(label="Update", type="primary", use_container_width=True)
+        # Remove tab
+        with self.layer_options_remove_tab:
+            self.layer_options_remove_warning_msg = st.warning("Choose a layer to delete from the map. This can not be undone.")
+            self.layer_options_remove_selected_layer = st.selectbox(label="Layer To Remove", options=st.session_state["layers_list"], help="Select the layer you wish to delete from the map, this will also delete all markers and zones associated with it.")
+            self.layer_options_remove_btn = st.form_submit_button(label="Delete", use_container_width=True)
         # Marker options form
         with self.marker_options_form:
-            self.marker_options_subheader = st.subheader("Marker Options")
-            self.marker_options_add_tab, self.marker_options_remove_tab = st.tabs(["Add", "Remove"])
+            self.marker_options_subheader = st.subheader("Markers")
+            self.marker_options_add_tab, self.marker_options_edit_tab, self.marker_options_remove_tab = st.tabs(["Add", "Edit", "Remove"])
+        # Add tab
         with self.marker_options_add_tab:
-            self.marker_name = st.text_input(label="Name (ID)", placeholder="Choose a name for the marker", help="Choose a name for the marker to be used as its ID. This will not be displayed on the map.")
-            self.marker_icon = st.selectbox(label="Icon", options=st.session_state["library_icons"], help="Choose icons from the library to use with your marker.")
-            self.marker_options_add_tab_lcol, self.marker_options_remove_tab_rcol = st.columns(2)
-            self.marker_popup_text = st.text_input(label="Popup Content", placeholder="Create some content for your popup", help="The text that will be displayed when the user clicks the marker and the popup shows. Leave empty to dsable the popup.")
-            self.marker_tooltip_text = st.text_input(label="Tooltip Content", placeholder="Create some content for your tooltip", help="The text that will be displayed when the user hovers over the marker and the tooltip shows. Leave empty to disable the tooltip.")
+            self.marker_options_add_info_msg = st.info("Give your new marker a name and define its position and icon to be used.")
+            self.marker_options_add_name = st.text_input(label="Name (ID)", key="add-marker-name", placeholder="Choose a name for the marker.", help="Choose a name for the marker to be used as its ID. This will not be displayed on the map.")
+            self.marker_options_add_icon = st.selectbox(label="Icon", key="add-marker-icon", options=st.session_state["library_icons"], help="Choose icons from the library to use with your marker.")
+            self.marker_options_add_tab_lcol, self.marker_options_add_tab_rcol = st.columns(2)
+            self.marker_options_add_popup_text = st.text_area(label="Popup Content", key="add-marker-popup", placeholder="Create some content for your popup, supports Markdown so you can add headers, images, links, etc.", help="The text that will be displayed when the user clicks the marker and the popup shows. Leave empty to dsable the popup.")
+            self.marker_options_add_tooltip_text = st.text_area(label="Tooltip Content", key="add-marker-tooltip", placeholder="Create some content for your tooltip, supports Markdown so you can add headers, images, links, etc.", help="The text that will be displayed when the user hovers over the marker and the tooltip shows. Leave empty to disable the tooltip.")
             self.marker_options_add_btn = st.form_submit_button(label="Add", type="primary", use_container_width=True)
         with self.marker_options_add_tab_lcol:
-            self.marker_lat = st.number_input(label="Lat", value=st.session_state["last_clicked"]["lat"], help="Latitude of the marker.")
-            self.marker_width = st.number_input(label="Width", min_value=0, value=30, help="Width of the marker in pixels.")
-        with self.marker_options_remove_tab_rcol:
-            self.marker_lng = st.number_input(label="Lng", value=st.session_state["last_clicked"]["lng"], help="Longitude of the marker.")
-            self.marker_height = st.number_input(label="Height", min_value=0, value=30, help="Height of the marker in pixels.")
+            self.marker_options_add_lat = st.number_input(label="Lat", key="add-marker-lat", value=st.session_state["last_clicked"]["lat"], help="Latitude of the marker.")
+            self.marker_options_add_width = st.number_input(label="Width", key="add-marker-width", min_value=0, value=30, help="Width of the marker in pixels.")
+        with self.marker_options_add_tab_rcol:
+            self.marker_options_add_lng = st.number_input(label="Lng", key="add-marker-lng", value=st.session_state["last_clicked"]["lng"], help="Longitude of the marker.")
+            self.marker_options_add_height = st.number_input(label="Height", key="add-marker-height", min_value=0, value=30, help="Height of the marker in pixels.")
+        # Edit tab
+        with self.marker_options_edit_tab:
+            self.marker_options_edit_info_msg = st.info("Edit the selected markers details.")
+            self.marker_options_edit_selected_marker = st.selectbox(label="Marker To Edit", options=st.session_state["markers_list"], help="Choose a marker from the map to edit its details.")
+            self.marker_options_edit_name = st.text_input(label="Name (ID)", key="edit-marker-name", placeholder="Choose a name for the marker.", help="Choose a name for the marker to be used as its ID. This will not be displayed on the map.")
+            self.marker_options_edit_icon = st.selectbox(label="Icon", key="edit-marker-icon", options=st.session_state["library_icons"], help="Choose icons from the library to use with your marker.")
+            self.marker_options_edit_tab_lcol, self.marker_options_edit_tab_rcol = st.columns(2)
+            self.marker_options_edit_popup_text = st.text_area(label="Popup Content", key="edit-marker-popup", placeholder="Create some content for your popup, supports Markdown so you can add headers, images, links, etc.", help="The text that will be displayed when the user clicks the marker and the popup shows. Leave empty to dsable the popup.")
+            self.marker_options_edit_tooltip_text = st.text_area(label="Tooltip Content", key="edit-marker-tooltip", placeholder="Create some content for your tooltip, supports Markdown so you can add headers, images, links, etc.", help="The text that will be displayed when the user hovers over the marker and the tooltip shows. Leave empty to disable the tooltip.")
+            self.marker_options_edit_btn = st.form_submit_button(label="Update", type="primary", use_container_width=True)
+        with self.marker_options_edit_tab_lcol:
+            self.marker_options_edit_lat = st.number_input(label="Lat", key="edit-marker-lat", value=st.session_state["last_clicked"]["lat"], help="Latitude of the marker.")
+            self.marker_options_edit_width = st.number_input(label="Width", key="edit-marker-width", min_value=0, value=30, help="Width of the marker in pixels.")
+        with self.marker_options_edit_tab_rcol:
+            self.marker_options_edit_lng = st.number_input(label="Lng", key="edit-marker-lng", value=st.session_state["last_clicked"]["lng"], help="Longitude of the marker.")
+            self.marker_options_edit_height = st.number_input(label="Height", key="edit-marker-height", min_value=0, value=30, help="Height of the marker in pixels.")
+        # Remove tab
         with self.marker_options_remove_tab:
             self.marker_options_remove_warning_msg = st.warning("Choose a marker to delete from the map. This can not be undone.")
-            self.remove_marker = st.selectbox(label="Marker To Remove", options=st.session_state["markers_list"], help="Choose a marker to delete from the map. This can not be undone.")
+            self.marker_options_remove_selected_marker = st.selectbox(label="Marker To Remove", options=st.session_state["markers_list"], help="Choose a marker to delete from the map. This can not be undone.")
             self.marker_options_remove_btn = st.form_submit_button(label="Remove", type="secondary", use_container_width=True)
         # Zone options form
         with self.zone_options_form:
-            self.zone_options_subheader = st.subheader("Zone Options")
-            self.zone_options_btn = st.form_submit_button(label="Add", type="primary", use_container_width=True)
+            self.zone_options_subheader = st.subheader("Zones")
+            self.zone_options_add_tab, self.zone_options_edit_tab, self.zone_options_remove_tab = st.tabs(["Add", "Edit", "Remove"])
+        # Add tab
+        with self.zone_options_add_tab:
+            self.zone_options_add_info_msg = st.info("Give your new zone a name and define its position and colour to be used.")
+            self.zone_options_add_btn = st.form_submit_button(label="Add", type="primary", use_container_width=True)
+        # Edit tab
+        with self.zone_options_edit_tab:
+            self.zone_options_edit_info_msg = st.info("Edit the selected zones details.")
+            self.zone_options_remove_selected_marker = st.selectbox(label="Zone To Edit", options=st.session_state["zones_list"], help="Choose a zone from the map to edit its details.")
+            self.zone_options_edit_btn = st.form_submit_button(label="Update", type="primary", use_container_width=True)
+        # Remove tab
+        with self.zone_options_remove_tab:
+            self.zone_options_remove_warning_msg = st.warning("Choose a zone to delete from the map. This can not be undone.")
+            self.zone_options_remove_selected_marker = st.selectbox(label="Zone To Remove", options=st.session_state["zones_list"], help="Choose a zone to delete from the map. This can not be undone.")
+            self.zone_options_remove_btn = st.form_submit_button(label="Remove", use_container_width=True)
 
 class EditMap():
     pass
